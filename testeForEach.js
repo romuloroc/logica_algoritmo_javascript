51b99410cf8434f1108d57bdd673f0e3a63f5644
Romulo Cardoso <romulo.cardoso@ipremios.com>
2022-07-08 09:23:01 -0300

    Changes

---
+++ testeForEach.js
@@ -0,0 +1,22 @@
+// const nomes = ['Teste', 'Bagulhos', 'Windows', 'Freeway'];
+
+// for(let i = 0; i < nomes.length; i++) {
+//     console.log(nomes[i],i)
+// }
+
+const nomes = ['Teste', 'Bagulhos', 'Windows', 'Freeway'];
+
+nomes.forEach(function(nome, i) {
+     console.log(nome, i)
+})
+
+function nossoForEach(arr, funcao) {
+    for(let i = 0; i < arr.length; i++) {
+        const intemDoArray = arr[i]
+        funcao(intemDoArray, i)
+    }
+}
+
+nossoForEachnomes(nomes, function(nome, i){
+    console.log(nome, i)
+})
