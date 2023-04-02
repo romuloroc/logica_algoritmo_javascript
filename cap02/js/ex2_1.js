da5a8dfa1ac188acac70ae36dee4208699a21d52
Romulo Cardoso <romulo.cardoso@ipremios.com>
2023-04-02 16:36:45 -0300

    changes

---
+++ cap02/js/ex2_1.js
@@ -0,0 +1,12 @@
+// Cria referencia ao form e ao elemento h3 (onde será exibida a resposta)
+
+const form = document.querySelector('form');
+const resp = document.querySelector('h3');
+
+// cria um "ouvinte" de evento, acionado quando o botão submit for clicado
+
+form.addEventListener("submit", (e) => {
+  const nome = form.inNome.value; // obtém o nome digitado no form
+  resp.innerText = `Olá ${nome}`  // exibe a resposta do programa
+  e.preventDefault();
+})
